cc8d77fc514331a77f39670e8833ab33e00a1b22
reevu chatterjee <chatterjeeireevu@gmail.com>
2025-06-30 11:48:01 +0530

    Added rescue model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+class RescueDB(models.Model):
+    contactName=models.CharField(max_length=150)
+    contactNumber=models.CharField(max_length=11)
+    location=models.TextField()
+    type=models.CharField()
+    photo=models.ImageField(upload_to='Rescue_Photo/', default='default.png', blank=True)
+    issueFaced=models.TextField()
+
+
 # Create your models here.
