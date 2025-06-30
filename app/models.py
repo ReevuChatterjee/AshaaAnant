4218c77b70d72d9cf9edc75b55de73b69d4b4e15
reevu chatterjee <chatterjeeireevu@gmail.com>
2025-06-30 19:44:44 +0530

    added auth model

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 class RescueDB(models.Model):
     contactName=models.CharField(max_length=150)
@@ -11,5 +12,9 @@ class RescueDB(models.Model):
 
     def __str__(self):
         return f"{self.contactName} - {self.type} - {self.timestamp.strftime('%Y-%m-%d %H:%M')}"
+    
+class CustomUser(AbstractUser):
+    def __str__(self):
+        return self.email or self.username
 
 # Create your models here.
