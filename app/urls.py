760e31bb6cc52d49ba3c8c2cd5b807541e380cc7
reevu chatterjee <chatterjeeireevu@gmail.com>
2025-06-30 10:42:35 +0530

    Added rescue page

--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('rescue/',views.rescue,name='rescue')
 ]
--- app/views.py
+++ app/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render, HttpResponse
 def home(request):
     return render(request,'home.html')
 
+def rescue(request):
+    return render(request,'rescue.html')
+
 # Create your views here.
