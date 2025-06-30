48a5473cfa382d791ee9eb4362f993e84c13d7fe
reevu chatterjee <chatterjeeireevu@gmail.com>
2025-06-30 13:51:17 +0530

    Model Backend

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('rescue/',views.rescue,name='rescue')
+    path('rescue/',views.rescue,name='rescue'),
+    path('submit-rescue/', views.submitRescueReport, name='submitRescue'),
+    path('adoption/',views.adoption,name='adoption'),
+    path('donate/',views.donate,name='donate'),
+    path('volunteer/',views.volunteer,name='volunteer'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse,redirect
+from .models import RescueDB
 
 def home(request):
     return render(request,'home.html')
@@ -6,4 +7,26 @@ def home(request):
 def rescue(request):
     return render(request,'rescue.html')
 
+def submitRescueReport(request):
+    if request.method=="POST":
+        contactName=request.POST.get('contactName')
+        contactNumber=request.POST.get('contactNumber')
+        address=request.POST.get('address')
+        type=request.POST.get('animalType')
+        photo=request.FILES.get('photo')
+        issue=request.POST.get('issue')
+        en=RescueDB(contactName=contactName,contactNumber=contactNumber,location=address,type=type,photo=photo,issueFaced=issue)
+        en.save()
+        return render(request, 'rescue.html', {'success': True})
+    
+    return redirect('rescue')
+
+def adoption(request):
+    return HttpResponse("ADOPTION")
+
+def donate(request):
+    return HttpResponse("DONATE")
+
+def volunteer(request):
+    return HttpResponse("VOLUNTEER")
 # Create your views here.
