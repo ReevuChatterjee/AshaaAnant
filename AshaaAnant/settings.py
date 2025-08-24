0d3ae01c226a7832dd6d296f7e577d8a9032dc0d
reevu chatterjee <chatterjeeireevu@gmail.com>
2025-08-24 13:31:30 +0530

    Resolved Deploy issue 1

--- AshaaAnant/settings.py
+++ AshaaAnant/settings.py
@@ -27,9 +27,9 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG","False").lower()=="true"
+DEBUG = os.environ.get("DEBUG")
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_SIGNUP_IPS")
 
 
 # Application definition
